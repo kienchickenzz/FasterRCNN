d150a83ff55840a65a2be04d8ff37ccfd0adf101
Nguyen Duc Kien <kienchickenz@gmail.com>
2025-08-23 17:12:17 +0700

    Refactor/Minor changes

--- pytorch/FasterRCNN/__main__.py
+++ pytorch/FasterRCNN/__main__.py
@@ -238,7 +238,7 @@ def predict(model, image_data, image, show_image, output_path):
     show_image = show_image,
     image = image,
     scored_boxes_by_class_index = scored_boxes_by_class_index,
-    class_index_to_name = voc.Dataset.class_index_to_name
+    class_index_to_name = dogcat.Dataset.class_index_to_name
   )
 
 def predict_one(model, url, show_image, output_path):
@@ -251,7 +251,7 @@ def predict_all(model, split):
   if not os.path.exists(dirname):
     os.makedirs(dirname)
   print("Rendering predictions from '%s' set to '%s'..." % (split, dirname))
-  dataset = voc.Dataset(
+  dataset = dogcat.Dataset(
     dir = options.dataset_dir,
     split = split,
     image_preprocessing_params = model.backbone.image_preprocessing_params,
@@ -267,32 +267,74 @@ def predict_all(model, split):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser("FasterRCNN")
   group = parser.add_mutually_exclusive_group()
-  group.add_argument("--train", action = "store_true", help = "Train model")
-  group.add_argument("--eval", action = "store_true", help = "Evaluate model")
-  group.add_argument("--predict", metavar = "url", action = "store", type = str, help = "Run inference on image and display detected boxes")
-  group.add_argument("--predict-to-file", metavar = "url", action = "store", type = str, help = "Run inference on image and render detected boxes to 'predictions.png'")
-  group.add_argument("--predict-all", metavar = "name", action = "store", type = str, help = "Run inference on all images in the specified dataset split and write to directory 'predictions_${split}'")
-  parser.add_argument("--load-from", metavar = "file", action = "store", help = "Load initial model weights from file")
-  parser.add_argument("--backbone", metavar = "model", action = "store", default = "vgg16", help = "Backbone model for feature extraction and classification")
-  parser.add_argument("--save-to", metavar = "file", action = "store", help = "Save final trained weights to file")
-  parser.add_argument("--save-best-to", metavar = "file", action = "store", help = "Save best weights (highest mean average precision) to file")
-  parser.add_argument("--dataset-dir", metavar = "dir", action = "store", default = "VOCdevkit/VOC2007", help = "VOC dataset directory")
-  parser.add_argument("--train-split", metavar = "name", action = "store", default = "trainval", help = "Dataset split to use for training")
-  parser.add_argument("--eval-split", metavar = "name", action = "store", default = "test", help = "Dataset split to use for evaluation")
-  parser.add_argument("--cache-images", action = "store_true", help = "Cache images during training (requires ample CPU memory)")
-  parser.add_argument("--periodic-eval-samples", metavar = "count", action = "store", type = int, default = 1000, help = "Number of samples to use during evaluation after each epoch")
-  parser.add_argument("--checkpoint-dir", metavar = "dir", action = "store", help = "Save checkpoints after each epoch to the given directory")
-  parser.add_argument("--plot", action = "store_true", help = "Plots the average precision of each class after evaluation (use with --train or --eval)")
-  parser.add_argument("--log-csv", metavar = "file", action = "store", help = "Log training metrics to CSV file")
-  parser.add_argument("--epochs", metavar = "count", type = int, action = "store", default = 1, help = "Number of epochs to train for")
-  parser.add_argument("--learning-rate", metavar = "value", type = float, action = "store", default = 1e-3, help = "Learning rate")
-  parser.add_argument("--momentum", metavar = "value", type = float, action = "store", default = 0.9, help = "Momentum")
-  parser.add_argument("--weight-decay", metavar = "value", type = float, action = "store", default = 5e-4, help = "Weight decay")
-  parser.add_argument("--dropout", metavar = "probability", type = float, action = "store", default = 0.0, help = "Dropout probability after each of the two fully-connected detector layers")
-  parser.add_argument("--no-augment", action = "store_true", help = "Disable image augmentation (random horizontal flips) during training")
-  parser.add_argument("--exclude-edge-proposals", action = "store_true", help = "Exclude proposals generated at anchors spanning image edges from being passed to detector stage")
-  parser.add_argument("--dump-anchors", metavar = "dir", action = "store", help = "Render out all object anchors and ground truth boxes from the training set to a directory")
-  parser.add_argument("--profile-cuda-memory", action = "store_true", help = "Profile CUDA memory usage and write output to 'cuda_memory.txt'")
+
+    # Run Mode
+  group.add_argument("--train", action = "store_true", 
+        help = "Train model")
+  group.add_argument("--eval", action = "store_true", 
+        help = "Evaluate model")
+  group.add_argument("--dump-anchors", metavar = "dir", action = "store", 
+        help = "Render out all object anchors and ground truth boxes from the training set to a directory" )
+  group.add_argument("--predict", metavar = "url", action = "store", type = str, 
+        help = "Run inference on image and display detected boxes" )
+  group.add_argument("--predict-to-file", metavar = "url", action = "store", type = str, 
+        help = "Run inference on image and render detected boxes to 'predictions.png'" )
+  group.add_argument("--predict-all", metavar = "name", action = "store", type = str, 
+        help = "Run inference on all images in the specified dataset split and write to directory 'predictions_${split}'" )
+    
+    # Model Configuration
+  parser.add_argument("--load-from", metavar = "file", action = "store", 
+        help = "Load initial model weights from file")
+  parser.add_argument("--backbone", metavar = "model", action = "store", default = "vgg16", 
+        help = "Backbone model for feature extraction and classification")
+  
+    # Output & Logging
+  parser.add_argument("--save-to", metavar = "file", action = "store", 
+        help = "Save final trained weights to file" )
+  parser.add_argument("--save-best-to", metavar = "file", action = "store", 
+        help = "Save best weights (highest mean average precision) to file" )
+  parser.add_argument("--checkpoint-dir", metavar = "dir", action = "store", 
+        help = "Save checkpoints after each epoch to the given directory" )
+  parser.add_argument("--log-csv", metavar = "file", action = "store", 
+        help = "Log training metrics to CSV file" )
+  
+    # Dataset Configuration
+  parser.add_argument("--dataset-dir", metavar = "dir", action = "store", default = "VOCdevkit/VOC2007", 
+        help = "Dataset directory" )
+  parser.add_argument("--train-split", metavar = "name", action = "store", default = "train", 
+        help = "Dataset split to use for training" )
+  parser.add_argument("--eval-split", metavar = "name", action = "store", default = "val", 
+        help = "Dataset split to use for evaluation" )
+  
+  
+    # Training Parameters
+  parser.add_argument("--epochs", metavar = "count", type = int, action = "store", default = 1, 
+        help = "Number of epochs to train for" )
+  parser.add_argument("--learning-rate", metavar = "value", type = float, action = "store", default = 1e-3, 
+        help = "Learning rate" )
+  parser.add_argument("--momentum", metavar = "value", type = float, action = "store", default = 0.9, 
+        help = "Momentum" )
+  parser.add_argument("--weight-decay", metavar = "value", type = float, action = "store", default = 5e-4, 
+        help = "Weight decay" )
+  parser.add_argument("--dropout", metavar = "probability", type = float, action = "store", default = 0.0, 
+        help = "Dropout probability after each of the two fully-connected detector layers" )
+  
+    # Training Options
+  parser.add_argument("--cache-images", action = "store_true", 
+        help = "Cache images during training (requires ample CPU memory)" )
+  parser.add_argument( "--no-augment", action = "store_true", 
+        help = "Disable image augmentation (random horizontal flips) during training" )
+  parser.add_argument( "--exclude-edge-proposals", action = "store_true", 
+        help = "Exclude proposals generated at anchors spanning image edges from being passed to detector stage" )
+  parser.add_argument("--periodic-eval-samples", metavar = "count", action = "store", type = int, default = 1000, 
+        help = "Number of samples to use during evaluation after each epoch" )
+  parser.add_argument("--plot", action = "store_true", 
+        help = "Plots the average precision of each class after evaluation (use with --train or --eval)" )
+  
+    # Debug & Profiling
+  parser.add_argument("--profile-cuda-memory", action = "store_true", 
+        help = "Profile CUDA memory usage and write output to 'cuda_memory.txt'" )
+  
   options = parser.parse_args()
 
   # Validate backbone model
@@ -317,7 +359,7 @@ if __name__ == "__main__":
 
   # Construct model and load initial weights
   model = FasterRCNNModel(
-    num_classes = voc.Dataset.num_classes,
+    num_classes = dogcat.Dataset.num_classes,
     backbone = backbone,
     allow_edge_proposals = not options.exclude_edge_proposals
   ).cuda()
